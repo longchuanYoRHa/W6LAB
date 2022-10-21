8650066b6cb896b358debdbb5a3d4486219d7629
Longchuan <2674312287@qq.com>
2022-10-21 15:03:21 +0800

    EX5-10

--- LAB/LAB/01.c
+++ LAB/LAB/01.c
@@ -17,12 +17,12 @@ int main()
 	}
 	printf("\n");
 	i = 0;
-	while ("\0");
-	{
-		printf(adr[i]);
-		i++;
-	}
-	printf("0\n");
+	//while (adr[i]);
+	//{
+	//	printf("%c",adr[i]);
+	//	i++;
+	//}
+	//printf("0\n");
 
 	//EX3
 	int digits[10] = { 0 };
@@ -37,5 +37,23 @@ int main()
 	index = --month;
 	printf("Month %2d has %d days\n", ++month, days[index]);
 
+	//EX5
+
+	int arr1[] = { 1,2,3,8.16,32 };
+	
+	//EX6
+
+	int arr2[100] = { 0 };
+	arr2[99] = -1;
+	printf("%d", arr2[99]);
+
+	//EX7
+	//EX9
+	//rebo 
+	index = 0;
+	int ref[] = { 9, 10, 20, 12, 101, 32 };
+	int* ptr;
+	for (index = 0, ptr = ref + 5; index < 6; index++, ptr--)
+		printf(" % d % d\n", ref[index], *ptr);
 	return 0;
 }
